3ecd5b1ee7ca6bbc9df5ba2b3eea2f615677f42f
Jeffrey Orndorff <jeffrey.orndorff@gmail.com>
2020-08-13 20:51:33 -0700

    eating_cookies now working properly for small and large tests

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,17 +2,26 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
+# TO-DO: need to implement a cache for large tests
+def eating_cookies(n, cache=[]):
     # base case
     if n == 0:
         return 1
-
     elif n < 0:
         return 0
-
     else:
+        if len(cache) == 0:
+            cache = [0] * (n+1)
+
+        if cache[n] != 0:
+            return cache[n]
+        
         # 3 recursive calls
-        return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+        cache[n] = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+        return cache[n]
+
+
+
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
